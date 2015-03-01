acf21ce9480cde05e4a5fe4654fe0e96d630040d
Tieks <Tieks@users.noreply.github.com>
2015-03-01 12:25:56 +0200

    congigurations.php

---
+++ configurations.sql
@@ -0,0 +1,6 @@
+CREATE TABLE `users` (
+`id` int(3) NOT NULL auto_increment,
+`login` varchar(8) default NULL,
+`password` varchar(8) default NULL,
+PRIMARY KEY (`id`)
+) TYPE=MyISAM AUTO_INCREMENT=3 ;
