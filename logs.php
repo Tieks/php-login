5264a06959ff263407908912969b9e74056c21e1
Tieks <Tieks@users.noreply.github.com>
2015-03-01 12:10:24 +0200

    logs.php

---
+++ logs.php
@@ -0,0 +1,29 @@
+<?
+session_name("Login");
+session_start();
+
+require("connect.php");
+
+if(mysql_num_rows($q_user) == 1) {
+
+$query = mysql_query("SELECT * FROM USERS WHERE login='$name'");
+$data = mysql_fetch_array($query);
+if($_POST['pwd'] == $data['password']) { 
+session_register("name");
+header("Location: yourpage.php"); // success page. put the URL you want 
+exit;
+} else {
+header("Location: login.php?login=failed&cause=".urlencode('Wrong Password'));
+exit;
+}
+} else {
+header("Location: login.php?login=failed&cause=".urlencode('Invalid User'));
+exit;
+}
+}
+
+// if the session is not registered
+if(session_is_registered("name") == false) {
+header("Location: login.php");
+}
+?>
